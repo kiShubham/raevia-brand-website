e672cc10937e0bb86a0f747857b7adca7a1e1871
ak007python <kadamaniket1301@gmail.com>
2026-01-02 23:11:04 +0530

    feat: remove location display from testimonial cards

--- src/components/sections/TestimonialsSection.tsx
+++ src/components/sections/TestimonialsSection.tsx
@@ -59,7 +59,7 @@ const TestimonialCard = ({
                 {testimonial.author}
               </cite>
               <span className="block text-xs text-muted-foreground mt-1">
-                {testimonial.location}
+                {/* {testimonial.location} */}
               </span>
             </div>
           </footer>
